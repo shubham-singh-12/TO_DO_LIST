de04b5842f09d94ca962d79715ccac6c4f880ed1
Shubham Singh <122069809+shubham-singh-12@users.noreply.github.com>
2023-06-05 18:27:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+document.querySelector("#enter").addEventListener("click", () => {
+  const item = document.querySelector("#item")
+  createItem(item)
+})
+
+document.querySelector("#item").addEventListener("keypress", (e) => {
+  if(e.key === "Enter"){
+    const item = document.querySelector("#item")
+    createItem(item)
+  }
+})
+
+
+
+// THIS FUNCTION IS USED TO DISPLAY CURRENT DATE ONLY 
+function displayDate(){
+  let date = new Date()
+  
+  // SPLIT DATE INTO ARRAY 
+  date = date.toString().split(" ")
+  
+  // DISPLAY DATE into SCREEN
+  date = date[1] + " " + date[2] + " " + date[3] 
+  document.querySelector("#date").innerHTML = date  
+}
+
+
+// FUNCTION to DISPLAY ITEMS on DISPLAY from ARRAY 
+function displayItems(){
+  let items = ""
+  for(let i = 0; i < itemsArray.length; i++){
+    items += `<div class="item">
+                <div class="input-controller">
+                  <textarea disabled>${itemsArray[i]}</textarea>
+                  <div class="edit-controller">
+                    <i class="fa-solid fa-check deleteBtn"></i>
+                    <i class="fa-solid fa-pen-to-square editBtn"></i>
+                  </div>
+                </div>
+                <div class="update-controller">
+                  <button class="saveBtn">Save</button>
+                  <button class="cancelBtn">Cancel</button>
+                </div>
+              </div>`
+  }
+  document.querySelector(".to-do-list").innerHTML = items
+  activateDeleteListeners()
+  activateEditListeners()
+  activateSaveListeners()
+  activateCancelListeners()
+}
+
+
+// FUNCTION to DELETE ITEMS from LIST 
+function activateDeleteListeners(){
+  let deleteBtn = document.querySelectorAll(".deleteBtn")
+  deleteBtn.forEach((dB, i) => {
+    dB.addEventListener("click", () => { deleteItem(i) })
+  })
+}
+
+
+// FUNCTION to ACTIVATE EDIT & DELETE EVENT
+function activateEditListeners(){
+  const editBtn = document.querySelectorAll(".editBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  editBtn.forEach((eB, i) => {
+    eB.addEventListener("click", () => { 
+      updateController[i].style.display = "block"
+      inputs[i].disabled = false })
+  })
+}
+
+
+// FNNCTION to SAVE CHANGES (SAVE BUTTON) on LISTS ITEMS 
+function activateSaveListeners(){
+  const saveBtn = document.querySelectorAll(".saveBtn")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener("click", () => {
+      updateItem(inputs[i].value, i)
+    })
+  })
+}
+
+
+// FUNCTION to CANCEL CHANGES (CANCEL BUTTON) on LIST ITEMS 
+function activateCancelListeners(){
+  const cancelBtn = document.querySelectorAll(".cancelBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  cancelBtn.forEach((cB, i) => {
+    cB.addEventListener("click", () => {
+      updateController[i].style.display = "none"
+      inputs[i].disabled = true
+      inputs[i].style.border = "none"
+    })
+  })
+}
+
+
+// FUNCTION to STORE DATA in LOCAL STORAGE 
+function createItem(item){
+  itemsArray.push(item.value)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+
+// FUNCTION to DELETE ITEM and also UPDATE LOCAL STORAGE
+function deleteItem(i){
+  itemsArray.splice(i,1)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+
+// FUNCTION to UPDATE ITEM and also UPDATE LOCAL STORAGE
+function updateItem(text, i){
+  itemsArray[i] = text
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+window.onload = function() {
+  displayDate()
+  displayItems()
+};
